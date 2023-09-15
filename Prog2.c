a3368a52400f0f63fc22da07a84a7507c53ebff6
Rahul <adityakashyap2501@gmail.com>
2023-09-15 10:45:34 +0530

    still getting error

--- Prog2.c
+++ Prog2.c
@@ -12,6 +12,20 @@ typedef struct node
     struct node* next;
 } node;
 
+void display(node* head){
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+
+    node* temp = head;
+    while(temp != NULL){
+        printf("%d -> ", temp->info);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
 void create_node(node* head){
     int x;
     char c;
@@ -29,18 +43,27 @@ void create_node(node* head){
         head->next = temp;
         create_node(temp);
     }
-    else if(c == 'n' || c == 'N'){
+    else{
         head->next = NULL;
     }
-    else{
-        printf("\n\n*******Please enter a valid input type!!*******\n\n");
-        while ((c = getchar()) != '\n' && c != EOF);
-    }
+    // else{
+    //     printf("\n\n*******Please enter a valid input type!!*******\n\n");
+    //     while ((c = getchar()) != '\n' && c != EOF);
+    // }
 
 }
 
-void insert_at_begin(node* head){
-
+void insert_at_begin(node* head, int data){
+    // if(head->next == NULL){
+    //     create_node(head);
+    // }
+    node* temp = head;
+    temp = (node*)malloc(sizeof(node));
+    temp->next = head;
+    temp->info = data;
+    head = temp;
+    printf("Link list after insertion at begin \n");
+    // display(head);
 }
 
 void insert_at_end(node* head){
@@ -63,26 +86,68 @@ void delete_at_location(node* head){
 
 }
 
-void display(node* head){
-    if (head == NULL) {
-        printf("List is empty.\n");
-        return;
-    }
 
-    node* temp = head;
-    while(temp != NULL){
-        printf("%d -> ", temp->info);
-        temp = temp->next;
-    }
-    printf("NULL\n");
-}
 
 
 int main(){
 
     node* head = (node*)malloc(sizeof(node));
-    create_node(head);
+    // create_node(head);
+    insert_at_begin(head,3);
+    int choice, data, position;
     display(head);
+
+    // while (1) {
+    //     printf("\nLinked List Operations:\n");
+    //     printf("1. Insert at the beginning\n");
+    //     printf("2. Insert at the end\n");
+    //     printf("3. Insert at a specific location\n");
+    //     printf("4. Delete at the beginning\n");
+    //     printf("5. Delete at the end\n");
+    //     printf("6. Display the list\n");
+    //     printf("7. Exit\n");
+    //     printf("Enter your choice: ");
+    //     scanf("%d", &choice);
+
+    //     switch (choice) {
+    //         case 1:
+    //             printf("Enter data to insert at the beginning: ");
+    //             scanf("%d", &data);
+    //             insert_at_begin(head, data);
+    //             break;
+    //         case 2:
+    //             printf("Enter data to insert at the end: ");
+    //             scanf("%d", &data);
+    //             // insertAtEnd(&head, data);
+    //             break;
+    //         case 3:
+    //             printf("Enter data to insert: ");
+    //             scanf("%d", &data);
+    //             printf("Enter the position (0-based) to insert at: ");
+    //             scanf("%d", &position);
+    //             // insertAtLocation(&head, data, position);
+    //             break;
+    //         case 4:
+    //             // deleteAtBeginning(&head);
+    //             break;
+    //         case 5:
+    //             // deleteAtEnd(&head);
+    //             break;
+    //         case 6:
+    //             display(head);
+    //             break;
+    //         case 7:
+    //             // Free memory and exit
+    //             while (head != NULL) {
+    //                 node* temp = head;
+    //                 head = head->next;
+    //                 free(temp);
+    //             }
+    //             exit(0);
+    //         default:
+    //             printf("Invalid choice. Please try again.\n");
+    //     }
+    // }
     
     return 0;
 }
