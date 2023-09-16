8e825d61d11a9f93d6a0cefce166f1a2d18f10da
Rahul <adityakashyap2501@gmail.com>
2023-09-16 23:19:41 +0530

    delete complete , sll complete

--- test.c
+++ test.c
@@ -120,6 +120,90 @@ Node *insert_at_location(Node *head, int data,int location)
     return head;
 }
 
+Node *delete_at_end(Node* head)
+{
+    if (head == NULL)
+    {
+        printf("List is empty!!\n");
+        return head;
+    }
+    else if(head->next == NULL){
+        free(head);
+        head = NULL;
+    }
+    else
+    {
+        Node *temp = head;
+        while (temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+        free(temp->next);
+        temp->next = NULL;
+    }
+
+    return head;
+}
+
+Node *delete_at_begin(Node* head)
+{
+    if (head == NULL)
+    {
+        printf("List is empty!!\n");
+        return head;
+    }
+    else if(head->next == NULL){
+        free(head);
+        head = NULL;
+    }
+    else
+    {
+        Node *temp = head;
+        head = temp->next;
+        free(temp);
+    }
+
+    return head;
+}
+
+
+Node *delete_at_location(Node* head, int location)
+{
+     if(location < 0){
+        printf("\nInvalid Position\n");
+        return head;
+    }
+    else if(location == 0){
+        head = delete_at_begin(head);
+        return head;
+    }
+
+    if (head == NULL)
+    {
+        printf("List is empty!!\n");
+        return head;
+    }
+    else
+    {
+        int count = 0;
+        Node *prev = head;
+        while (prev != NULL && count < location-1)
+        {
+            prev = prev->next;
+            count++;
+        }
+        if (prev == NULL || prev->next == NULL) {
+            printf("Invalid position.\n");
+            return head;
+        }
+        Node* curr = prev->next;
+        prev->next = curr->next;
+        free(curr);
+    }
+
+    return head;
+}
+
 int main()
 {
 
@@ -132,22 +216,23 @@ int main()
     {
         printf("Enter data for node : ");
         scanf("%d", &data);
-        printf("enter position where node is to be inserted (0-based Indexing): ");
-        scanf("%d", &position);
-        printf("Are you sure you want to insert node at %d position (y/n) : ",position);
-        scanf(" %c", &position_choice);
-        if(position_choice == 'n'){
-            printf("Renter position of node where you want to insert node : ");
-            scanf(" %d", &position);
-        }else if (position_choice != 'y')
-        {
-            printf("Invalid Input\n");
-            printf("Starting Again...\n");
-            continue;
-        }
+        // printf("enter position where node is to be inserted (0-based Indexing): ");
+        // scanf("%d", &position);
+        // printf("Are you sure you want to insert node at %d position (y/n) : ",position);
+        // scanf(" %c", &position_choice);
+        // if(position_choice == 'n'){
+        //     printf("Renter position of node where you want to insert node : ");
+        //     scanf(" %d", &position);
+        // }else if (position_choice != 'y')
+        // {
+        //     printf("Invalid Input\n");
+        //     printf("Starting Again...\n");
+        //     continue;
+        // }
         
         // head = insert_at_begin(head, data);
-        head = insert_at_location(head, data,position);
+        head = insert_at_end(head, data);
+        // head = insert_at_location(head, data,position);
 
         printf("Nodes after insertion : ");
         display(head);
@@ -179,6 +264,28 @@ int main()
             printf("Invalid Input\n");
         }
     }
+    // head = delete_at_begin(head);
+    // display(head);
+
+    // head = delete_at_begin(head);
+    // display(head);
+    // head = delete_at_begin(head);
+
+    printf("enter position of delete node : ");
+    scanf(" %d", &position);
+    head = delete_at_location(head,position);
+    display(head);
+
+    printf("enter position of delete node : ");
+    scanf(" %d", &position);
+    head = delete_at_location(head,position);
+    display(head);
+
+    printf("enter position of delete node : ");
+    scanf(" %d", &position);
+    head = delete_at_location(head,position);
+    display(head);
+
     printf("\nFinal Linked List : ");
     display(head);
     return 0;
